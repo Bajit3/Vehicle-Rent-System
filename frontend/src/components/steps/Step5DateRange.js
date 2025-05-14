dc960833356560b1bc0e5e97f71082502a636f74
Ajit Borgude <ajitborgude1379@gmail.com>
2025-05-14 23:11:55 +0530

    fixed end date issue

--- frontend/src/components/steps/Step5DateRange.js
+++ frontend/src/components/steps/Step5DateRange.js
@@ -1,31 +1,34 @@
 import { useFormContext } from 'react-hook-form';
 import { DatePicker } from '@mui/x-date-pickers';
-import { Stack, Typography } from '@mui/material';
+import { Stack } from '@mui/material';
 import dayjs from 'dayjs';
 
 const Step5DateRange = () => {
-  const { setValue, watch, formState: { errors } } = useFormContext();
+  const { setValue, watch } = useFormContext();
   const startDate = watch('startDate');
   const endDate = watch('endDate');
-  console.log('endDate: ', endDate);
 
   return (
     <Stack spacing={3} sx={{ mt: 2 }}>
-     <DatePicker
-  label="Start Date"
-  value={startDate ? dayjs(startDate) : null}
-  onChange={(date) => setValue('startDate', date?.toDate(), { shouldValidate: false })}  // Ensure no validation on initial render
-  slotProps={{
-    textField: {
-      variant: 'outlined',
-      fullWidth: true,
-    },
-  }}
-/>
-      <DatePicker 
-        label="End Date" 
-        value={endDate ? dayjs(endDate) : null} 
+      <DatePicker
+        label="Start Date"
+        value={startDate ? dayjs(startDate) : null}
+        onChange={(date) =>
+          setValue('startDate', date?.toDate(), { shouldValidate: false })
+        }
+        slotProps={{
+          textField: {
+            variant: 'outlined',
+            fullWidth: true,
+          },
+        }}
+      />
+
+      <DatePicker
+        label="End Date"
+        value={endDate ? dayjs(endDate) : null}
         onChange={(date) => setValue('endDate', date?.toDate())}
+        minDate={startDate ? dayjs(startDate) : undefined} 
         slotProps={{
           textField: {
             variant: 'outlined',
